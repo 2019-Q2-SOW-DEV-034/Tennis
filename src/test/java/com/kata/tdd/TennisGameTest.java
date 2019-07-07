145e0a2a7b50e07f8c0b89b107aa4a375b29acf9
unknownUser <unknown.usermail@gmail.com>
2019-07-07 21:06:36 +0200

    Refactored : introduced @BeforeEach in TennisGameTest

--- src/test/java/com/kata/tdd/TennisGameTest.java
+++ src/test/java/com/kata/tdd/TennisGameTest.java
@@ -1,5 +1,6 @@
 package com.kata.tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,27 +8,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TennisGameTest {
 
+    private TennisGame tennisGame;
+
+    @BeforeEach
+    public void initializeBeforeEachMethod(){
+        tennisGame = new TennisGame();
+    }
+
+
     @Test
     @DisplayName("When the Tennis Game start, player one scored point should be equal to zero")
     public void whenTheGameStartsPlayerOneScoredPointShouldBeEqualToZeroTest(){
-        TennisGame tennisGame = new TennisGame();
-
         assertEquals(0, tennisGame.getPlayerOneScoredPoint());
     }
 
     @Test
     @DisplayName("When the Tennis Game start, player two scored point should be equal to zero")
     public void whenTheGameStartsPlayerTwoScoredPointShouldBeEqualToZeroTest(){
-        TennisGame tennisGame = new TennisGame();
-
         assertEquals(0, tennisGame.getPlayerTwoScoredPoint());
     }
 
     @Test
     @DisplayName("When the Tennis Game start, player one name should be initialized as 'Player1'")
     public void whenTheGameStartsPlayerOneNameShouldBeInitializedAsPlayer1Test(){
-        TennisGame tennisGame = new TennisGame();
-
         assertEquals("Player1", tennisGame.getPlayerOneName());
     }
 }
